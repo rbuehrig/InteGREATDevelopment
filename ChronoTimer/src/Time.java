8b085941a2b072434a5ad1c5aa52e61ee768d819
Philip Kocol <pmkocol@uwm.edu>
2017-03-01 13:06:51 -0600

    3/1/17, 1:06 p.m. -Added documentation for methods -Added start() and finish method overloads -Added parseMilli() method

--- ChronoTimer/src/Time.java
+++ ChronoTimer/src/Time.java
@@ -7,48 +7,127 @@ import java.util.Queue;
 public class Time {
 	private LinkedList<Long> startTimes;
 	private LinkedList<Long> racerTimes;
-	//private LinkedList<Integer> racers;
 	
 	private Clock clock;
 	
 	public Time(){
 		startTimes = new LinkedList<Long>();
 		racerTimes = new LinkedList<Long>();
-		//racers = new LinkedList<Integer>();
 	}
 	
+	/**
+	 * Default method for a start event
+	 * Adds current time to start list 
+	 * 
+	 * @author Philip Kocol
+	 * @return clock.millis()
+	 */
 	public long start(){
 		long start = clock.millis();
 		startTimes.add(start);
-		//racers.add(racer);
 		return start;
 	}
 	
+	/**
+	 * Overload for start()
+	 * Allows for user input as start time
+	 * 
+	 * @author Philip Kocol
+	 * @param start
+	 * @return start time that was added
+	 */
+	public long start(long start){
+		startTimes.add(start);
+		return start;
+	}
+	
+	/**
+	 * Default method for finish event
+	 * Calculates time for next racer in the the queue and adds it to a list of race times
+	 * 
+	 * @author Philip Kocol
+	 * @return finish time of next racer
+	 * @throws NoSuchElementException
+	 */
 	public long finish(){
 		if(startTimes.isEmpty()) throw new NoSuchElementException("No racers started");
 		
 		long finish = clock.millis();
 		long start = startTimes.pollFirst();
-		racerTimes.add(finish - start);
-		return start;
+		long time = finish - start;
+		racerTimes.add(time);
+		return time;
 	}
 	
-	/*Sets next racer to finish's time to -1 representing DNF*/
+	/**
+	 * Overload for finish()
+	 * Allows for user suppplied finish time
+	 * 
+	 * @author Philip Kocol
+	 * @param finish
+	 * @return finish time of next racer
+	 * @throws NoSuchElementException
+	 */
+	public long finish(long finish){
+		if(startTimes.isEmpty()) throw new NoSuchElementException("No racers started");
+		
+		long start = startTimes.pollFirst();
+		long time = finish - start;
+		racerTimes.add(time);
+		return time;
+		
+	}
+	
+	/**
+	 * Sets next racer time to -1, signifying a DNF or "did not finish"
+	 * 
+	 * @author Philip Kocol
+	 */
 	public void dnf(){
 		startTimes.remove();
 		racerTimes.add((long)-1);
 	}
 	
-	/*Completely removes start time of most recently added racer*/
+	/**
+	 * Removes start time of last racer put in the queue
+	 * Signifies a "cancel"
+	 * 
+	 * @author Philip Kocol
+	 */
 	public void cancel(){
 		startTimes.removeLast();
 	}
 	
-	/*Returns list of racer times in order of when each finished*/
+	/**
+	 * Returns a list of racer times in order of when the start gate was triggered
+	 * 
+	 * @return LinkedList of racer times
+	 */
 	public LinkedList<Long> getTimes(){
 		return racerTimes;
 	}
 	
+	/**
+	 * Parses a string of format "HH:MM:SS.SS" into milliseconds for time calculation
+	 * 
+	 * @author Philip Kocol
+	 * @param time
+	 * @return time in milliseconds since 00:00:00.00
+	 */
+	public long parseMilli(String time){
+		String[] parts = time.split(":");
+		long hour = Long.parseLong(parts[0]);
+		long minute = Long.parseLong(parts[1]);
+		String second = parts[2];
+		String[] secondParts = second.split(".");
+		long second1 = Long.parseLong(secondParts[0]);
+		long second2 = Long.parseLong(secondParts[1]); 
+		
+		
+		long parsedTime = (hour * 3600000) + (minute * 60000) + (second1 * 1000) + (second2 * 10);
+		return parsedTime;
+		
+	}
 	
 
 }
